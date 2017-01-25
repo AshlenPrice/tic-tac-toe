8c3cb82955932f2f7a35c75ce6e38e149308d9fe
ashlenprice <ashlen.price@gmail.com>
2017-01-24 21:22:07 -0500

    Create basic elements of the app
    This is not your best commit, try to commit often!
    Updated the following on the project:
    
    - assets/scripts/index.js - In this file I updated the code to match
    that of the css layout bootstrap SASS lesson.
    
    - assets/styles/index.scss - In this file I added some basic SASS
    from the css layout bootstrap SASS lesson.
    
     - index.html - In this file I added a basic 3x3 grid, a nav bar, the
     auth forms as well as a page title.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,8 +7,15 @@ $(() => {
   setAPIOrigin(location, config);
 });
 
+// when form is submitted then console log the input
+$("form").on("submit", function(event){
+  event.preventDefault();
+  let input = $("#name").val();
+  console.log(input);
+});
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+// require('./example');
