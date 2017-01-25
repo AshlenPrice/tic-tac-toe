a08ef76a86d47be6507fd8177787670e7db99768
ashlenprice <ashlen.price@gmail.com>
2017-01-25 12:29:13 -0500

    Fix auth issue.
    Was unable to sign in and generate token. Revised code in all the files.
    As well as created a new board.js file to start working on game logic.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const config = {
-  apiOrigins: {
-    production: 'https://aqueous-atoll-85096.herokuapp.com/',
+  apiOrigins : {
+    production: 'https://aqueous-atoll-85096.herokuapp.com',
+    development: 'http://tic-tac-toe.wdibos.com',
   },
 };
 
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,14 +8,10 @@ $(() => {
 });
 
 // when form is submitted then console log the input
-$("form").on("submit", function(event){
-  event.preventDefault();
-  let input = $("#name").val();
-  console.log(input);
-});
+const authEvents = require('./auth/events.js');
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-// require('./example');
+$(() => {
+  authEvents.addHandlers();
+});
