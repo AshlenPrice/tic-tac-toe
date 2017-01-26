ffe4f1385c319404348a0df4fbfb78462e872689
ashlenprice <ashlen.price@gmail.com>
2017-01-26 15:53:22 -0500

    create move counter
    Updated comments on engine to help creating other functions

--- assets/scripts/board.js
+++
@@ -1,6 +0,0 @@
-// make a constructor funtion for the board object
-// make functions for the things that need to happen ing the game
-// prototype the methods it will need
-// setup event listeners : what happens when i clicks in index.js
-
-// in the events.js setup game handles
--- assets/scripts/game/engine.js
+++ assets/scripts/game/engine.js
@@ -1,1 +1,73 @@
 'use strict';
+const newBoard = ["","","","","","","","","",""];
+//create the gameboard as an array object
+
+
+//
+const Game = function (board) {
+  this.board = board;
+  this.currentPlayer = 'x';
+  this.currentGameMoves = 0;
+  this.wins = [
+        [0,1,2],[3,4,5],[6,7,8], // Row
+        [0,3,6],[1,4,7],[2,5,8], // col
+        [0,4,8],[2,4,6]]; // diag
+
+};
+
+//establish a turn counter
+//establish player wining or losing
+/*Game states/statuses
+  not started
+  inProgress
+  winner=X O
+  gameTied
+*/
+
+
+//establishes moves.
+Game.prototype.setMove = function (i){
+  this.board[i] = this.currentPlayer;
+};
+// make a method on the Game object that changes the player
+Game.prototype.changePlayer =
+
+//
+
+
+//full board function
+isFull;: function() {
+  for(var i = 0; i < 9; i++) {
+    if (this.board[i] === this.EMPTY) {
+      return false;
+    }
+  }
+  return true;
+},
+
+// make functions for the things that need to happen in the game
+// prototype the methods it will need
+// setup event listeners : what happens when i clicks in index.js
+
+// in the events.js setup game handles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+newBoard,
+Game,
+
+};
