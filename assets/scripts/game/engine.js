71f55ce1e422fc2b899d61fdfd8ae9c975e3919c
ashlenprice <ashlen.price@gmail.com>
2017-01-29 18:45:36 -0500

    Engine complete
    fixed full board function
    used beautify to fix indentations

--- assets/scripts/game/engine.js
+++ assets/scripts/game/engine.js
@@ -6,50 +6,50 @@
 // let newGame = ('#reset');
 
 
-const Game = function () {
-  this.board = ['','','','','','','','',''];
+const Game = function() {
+  this.board = ['', '', '', '', '', '', '', '', ''];
   this.currentPlayer = 'X';
   this.currentGameMoves = 0;
   this.activeGame = true;
   this.gameOver = false;
   this.possibleWins = [
-                [0,1,2],[3,4,5],[6,7,8], // Row
-                [0,3,6],[1,4,7],[2,5,8], // col
-                [0,4,8],[2,4,6] // diag
-              ];
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Row
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // col
+    [0, 4, 8],
+    [2, 4, 6] // diag
+  ];
 };
 //prototype the methods the of the game
 
 // Need to determine if the board is full.
 // full board function
 
-//
-// Game.prototype.fullBoard = function() {
-//   for(let i = 0; i < 9; i++) {
-//     if (this.board[i] === '') {
-//       return false;
-//   } this.gameOver = true;
-//   console.log('The game is a draw');// need to change something here. Printing the game is a draw after every change turn
-//   return true;
-// }
-// };
 Game.prototype.fullBoard = function() {
-  for (let i = 0; i < 9; i++) {
-    if (this.board[i] === '') {
-      console.log(false);
-      return false;
-    } else if ((this.board[i] !== '') && (this.winner === false)) {
-      this.gameOver = true;
-      console.log('The game is a draw');
+  if (!this.gameOver) {
+    for (let i = 0; i < 9; i++) {
+      if (this.board[i] === '') {
+        return false;
+      }
     }
+    this.gameOver = true;
+    console.log('The game is a draw'); // need to change something here. Printing the game is a draw after every change turn
+    return true;
   }
 };
 
+// Game.prototype.fullBoard = function() {
+//   return !this.board.includes('');
+// };
+
 
 //establishes who moves.
-Game.prototype.setMove = function (i) {
-//is a condition that means is the game space is empty and it allows the rest of the funtion to run.
-  if (this.board[i]===''){
+Game.prototype.setMove = function(i) {
+  //is a condition that means is the game space is empty and it allows the rest of the funtion to run.
+  if (this.board[i] === '' && !this.gameOver) {
     this.board[i] = this.currentPlayer;
     this.winner();
     this.fullBoard();
@@ -57,17 +57,17 @@ Game.prototype.setMove = function (i) {
   }
 };
 // gameover should res
-Game.prototype.resetGameBoard = function () {
+Game.prototype.resetGameBoard = function() {
 
 };
 
 // this function should change the player after a move has been made by the opposite player
 
-Game.prototype.changePlayer = function (){
-  if (this.currentPlayer === 'X'){
-      this.currentPlayer = 'O';
+Game.prototype.changePlayer = function() {
+  if (this.currentPlayer === 'X') {
+    this.currentPlayer = 'O';
   } else {
-      this.currentPlayer = 'X';
+    this.currentPlayer = 'X';
   }
   return this.currentPlayer;
 };
@@ -78,20 +78,19 @@ Game.prototype.changePlayer = function (){
 /* this function should: check for a winner if a player has 3 matching indexes in a row that
 match any of the wins array */
 
-Game.prototype.winner = function () {
-  if
-     ((this.board[0]=== this.currentPlayer && this.board[1] === this.currentPlayer && this.board[2]=== this.currentPlayer) ||
-      (this.board[3]=== this.currentPlayer && this.board[4] === this.currentPlayer && this.board[5]=== this.currentPlayer) ||
-      (this.board[6]=== this.currentPlayer && this.board[7] === this.currentPlayer && this.board[8]=== this.currentPlayer) ||
-      (this.board[0]=== this.currentPlayer && this.board[3] === this.currentPlayer && this.board[6]=== this.currentPlayer) ||
-      (this.board[1]=== this.currentPlayer && this.board[4] === this.currentPlayer && this.board[7]=== this.currentPlayer) ||
-      (this.board[2]=== this.currentPlayer && this.board[5] === this.currentPlayer && this.board[8]=== this.currentPlayer) ||
-      (this.board[0]=== this.currentPlayer && this.board[4] === this.currentPlayer && this.board[8]=== this.currentPlayer) ||
-      (this.board[2]=== this.currentPlayer && this.board[4] === this.currentPlayer && this.board[6]=== this.currentPlayer)){
-        console.log(this.currentPlayer + ' is the WINNER!');
-        this.gameOver = true;
-        return this.currentPlayer + 'is the WINNER!';
-}
+Game.prototype.winner = function() {
+  if ((this.board[0] === this.currentPlayer && this.board[1] === this.currentPlayer && this.board[2] === this.currentPlayer) ||
+    (this.board[3] === this.currentPlayer && this.board[4] === this.currentPlayer && this.board[5] === this.currentPlayer) ||
+    (this.board[6] === this.currentPlayer && this.board[7] === this.currentPlayer && this.board[8] === this.currentPlayer) ||
+    (this.board[0] === this.currentPlayer && this.board[3] === this.currentPlayer && this.board[6] === this.currentPlayer) ||
+    (this.board[1] === this.currentPlayer && this.board[4] === this.currentPlayer && this.board[7] === this.currentPlayer) ||
+    (this.board[2] === this.currentPlayer && this.board[5] === this.currentPlayer && this.board[8] === this.currentPlayer) ||
+    (this.board[0] === this.currentPlayer && this.board[4] === this.currentPlayer && this.board[8] === this.currentPlayer) ||
+    (this.board[2] === this.currentPlayer && this.board[4] === this.currentPlayer && this.board[6] === this.currentPlayer)) {
+    console.log(this.currentPlayer + ' is the WINNER!');
+    this.gameOver = true;
+    return this.currentPlayer + 'is the WINNER!';
+  }
 };
 
 
@@ -104,49 +103,6 @@ Game.prototype.winner = function () {
   let the winning array
 */
 
-Game.prototype.getIndexes = function () {
-  console.log(this.wins);
-  let indexes = [];
-  for (let i = 0; i < this.board.length; i++) {
-    if (this.board[i] === this.currentPlayer) {
-      indexes.push(i);
-    }
-  }
-// this should push to the array .
-  return indexes;
-};
-
-
-// Game.prototype.winning = function (indx, callback) {
-//   const indexes = this.getIndexes();
-//   let winner = null;
-//
-//   if (this.boardFull()) {
-//     winner = 'tie';
-//   }
-//
-//   for (let i = 0; i < this.winningStates.length; i++) {
-//     const isWinner = this.compareArrays(indexes, this.winningStates[i]);
-//
-//     if (isWinner) {
-//       winner = this.currentPlayer;
-//       this.gameOver = true;
-//       break;
-//     }
-//   }
-//
-//   callback(winner, indx);
-// };
-
-//this resets the gameboard.
-  //needs to be tested in node
-
-
-//
-
-//clear the board
-//function that resets the board. Can this be turned into a method?
-
 
 // this creates the new game
 /*
@@ -158,12 +114,12 @@ let newGame = new Game();
 */
 
 function newGame() {
-let newGame = new Game();
- return newGame;
+  let newGame = new Game();
+  return newGame;
 }
-
+console.log('engine-works');
 
 module.exports = {
-Game,
-newGame,
+  Game,
+  newGame,
 };
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,7 +3,7 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 // const authEvents = require('./auth/events');
-// const gameEvents = require('./game/game-events');
+// const gameEvents = require('./game/events');
 
 $(() => {
   setAPIOrigin(location, config);
